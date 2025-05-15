95186a12cc64edbcc3c50f70573d058819d55c30
Erliandikasyahputraa <syahputraerliandika@gmail.com>
2025-05-15 23:35:54 +0700

    Initial push to FE-Emotica

---
+++ pages/main.js
@@ -0,0 +1,8 @@
+// Import CSS
+import './style.css'
+
+// Your JavaScript code here
+document.addEventListener('DOMContentLoaded', () => {
+    const app = document.getElementById('app')
+    app.innerHTML = '<h1>Welcome to Emotica!</h1>'
+}) 
